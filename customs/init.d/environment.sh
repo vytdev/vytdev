adad5407fd176ac9191a5111121b5d53c2128f09
Vincent Yanzee J. Tan <vytdev@gmail.com>
2025-05-17 06:34:04 +0800

    more env vars

--- customs/init.d/environment.sh
+++ customs/init.d/environment.sh
@@ -2,6 +2,7 @@
 
 set +e
 
+# locale
 export LANG=en_US.UTF-8
 
 # paths
@@ -11,21 +12,31 @@ if [ -z "$MANPATH" ] ; then
 fi
 
 # command history
-export HISTSIZE=5000
-export HISTFILESIZE=10000
+HISTSIZE=5000
+HISTFILESIZE=10000
+HISTCONTROL=ignoredups:erasedups
+HISTIGNORE='ls:cd:pwd:exit:clear'
+shopt -s histappend
 
 # setup gnupg
 export GPG_TTY=$(tty)
 export GNUPGHOME="$HOME/.gnupg"
 
-# some scripts might expect vim..
+# editor and pager
 export EDITOR=vim
 export VISUAL=vim
-
-# more or less...
 export PAGER=less
+
+# git editor and pager
+export GIT_EDITOR=vim
+export GIT_PAGER=less
+
+# other opts
+export LESS="-R"  # show ansi colours in `less`
+eval "$(dircolors -b)"
 
 # starting dir
 shopt -q login_shell && cd ~/projects
 
+# completions
 #. /etc/bash_completion.d/global-python-argcomplete
