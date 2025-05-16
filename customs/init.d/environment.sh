3c02b1d7dd5674baf6c51cea99cec32723046052
Vincent Yanzee J. Tan <vytdev@gmail.com>
2025-05-16 22:58:41 +0800

    update env script

--- customs/init.d/environment.sh
+++ customs/init.d/environment.sh
@@ -1,10 +1,31 @@
 # Setup the environment.
-export PATH=$PATH:~/.local/bin
+
+set +e
+
+export LANG=en_US.UTF-8
+
+# paths
+export PATH="$HOME/bin:$HOME/.local/bin:$PATH"
+if [ -z "$MANPATH" ] ; then
+  export MANPATH="$HOME/.local/man:$HOME/.local/share/man:$(manpath)"
+fi
+
+# command history
+export HISTSIZE=5000
+export HISTFILESIZE=10000
+
+# setup gnupg
 export GPG_TTY=$(tty)
-export GNUPGHOME=$HOME/.gnupg
+export GNUPGHOME="$HOME/.gnupg"
 
 # some scripts might expect vim..
 export EDITOR=vim
 export VISUAL=vim
+
+# more or less...
+export PAGER=less
+
+# starting dir
+shopt -q login_shell && cd ~/projects
 
 #. /etc/bash_completion.d/global-python-argcomplete
