8cd7345222159fc8ec4ceeebe7ee748feedd615a
Vincent Yanzee J. Tan <vytdev@gmail.com>
2025-05-19 09:51:13 +0800

    path completions

--- nvim/lua/configs/lsp.lua
+++ nvim/lua/configs/lsp.lua
@@ -17,6 +17,7 @@ cmp.setup({
     { name = 'nvim_lsp' },
     { name = 'nvim_lsp_signature_help' },
     { name = 'luasnip' },
+    { name = 'path' },
   }),
 })
 
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -25,6 +25,7 @@ require('lazy').setup({
   -- autocompletion, snippets, and lsp
   { 'hrsh7th/nvim-cmp' },
   { 'hrsh7th/cmp-nvim-lsp' },
+  { 'hrsh7th/cmp-path' },
   { 'hrsh7th/cmp-nvim-lsp-signature-help' },
   { 'saadparwaiz1/cmp_luasnip' },
   { 'L3MON4D3/LuaSnip' },
