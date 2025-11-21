96e4b458741589e3cb3a9e331d3f9b822056a590
Vincent Yanzee J. Tan <vytdev@gmail.com>
2025-11-21 07:52:06 +0000

    make the motd more neutral

--- customs/init.d/motd.sh
+++ customs/init.d/motd.sh
@@ -4,9 +4,6 @@
 echo
 echo "  Hi, vytdev!"
 echo
-echo "  Just live it! Our lives matter more than anything else."
-echo "  Never get into the industrial trap!!"
-echo
 echo "  A legend once said:"
 echo
 echo "    For what is a man, what has he got"
@@ -17,14 +14,13 @@ echo "    The record shows, I took the blows"
 echo "    And did it, My Way!"
 echo
 echo "    -- Frank Sinatra"
-echo
 
 # Color palette.
-# cnt=0
-# while [ "$cnt" -lt 16 ]; do
-#   [ $((cnt % 8)) -eq 0 ] && printf "\n    "
-#   printf "\033[48;5;%sm  \033[0m" "$cnt"
-#   cnt=$((cnt + 1))
-# done
-# echo
-# unset cnt
+cnt=0
+while [ "$cnt" -lt 16 ]; do
+  [ $((cnt % 8)) -eq 0 ] && printf "\n    "
+  printf "\033[48;5;%sm  \033[0m" "$cnt"
+  cnt=$((cnt + 1))
+done
+printf "\n\n"
+unset cnt
